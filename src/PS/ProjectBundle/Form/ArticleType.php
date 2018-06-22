ac557b12eb76b7fb54ea4894a1d125af0c78859d
jeromePALOS <jeromepalos@gmail.com>
2018-06-22 10:43:13 +0200

    add files in Project and Article

--- src/PS/ProjectBundle/Form/ArticleType.php
+++ src/PS/ProjectBundle/Form/ArticleType.php
@@ -23,6 +23,7 @@ class ArticleType extends AbstractType
         $builder
 			->add('title',     						TextType::class, array('required' => false))
 			->add('text',     						TextareaType::class, array('required' => false))
+            ->add('files', 				FilesType::class, array('required' => false, 'label' => false))
 			->add('Save the article',     			SubmitType::class)
 		;
     }/**
