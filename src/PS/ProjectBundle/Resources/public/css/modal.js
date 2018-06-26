501d9e1015051fa62097045dece3b6c77a0e968b
jeromePALOS <jeromepalos@gmail.com>
2018-06-26 11:49:25 +0200

    fadeout flash msg

--- src/PS/ProjectBundle/Resources/public/css/modal.js
+++ src/PS/ProjectBundle/Resources/public/css/modal.js
@@ -16,4 +16,22 @@ document.getElementById('modal-hide').addEventListener('click',
     }, 
 false);
 
-    
+
+	var modal = document.querySelectorAll(".modal-a");
+	for (var i = 0; i < modal.length; i++) {
+	   modal[i].addEventListener('click', 		
+		 function ConfirmForm() {
+		   document.getElementById("modal-text").textContent = this.dataset.text;
+		   document.getElementById("modal-title").textContent = this.dataset.title;
+		   document.getElementById("modal-input").setAttribute('href', this.dataset.input);
+		   document.getElementById("BlockUIConfirm").style.display = "block";
+
+		 }, 
+	  false);
+	}
+
+	document.getElementById('modal-hide').addEventListener('click', 		
+		function(){
+			document.getElementById("BlockUIConfirm").style.display = "none";
+		}, 
+	false);
